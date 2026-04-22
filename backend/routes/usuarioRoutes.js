f9b2f1298187fbc0fc067ef4abe0a452cf18af74
matheus110506 <matheus110506@hotmail.com>
2026-04-22 13:00:03 -0300

    Add '/me' route to get current user information

--- backend/routes/usuarioRoutes.js
+++ backend/routes/usuarioRoutes.js
@@ -1,9 +1,13 @@
 import express from "express";
+import { autenticar } from "../auth.js";
 import userController from "../controller/usuarioController.js";
 
 const router = express.Router();
 
 router.post("/register", userController.register);
 router.post("/login", userController.login);
+router.get("/me", autenticar, (req, res) => {
+  res.json(req.session.usuario);
+});
 
 export default router;
