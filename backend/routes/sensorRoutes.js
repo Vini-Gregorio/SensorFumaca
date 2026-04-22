71ca81e6dc7e70e9f2f7083ce3f49acf8355911b
matheus110506 <matheus110506@hotmail.com>
2026-04-22 12:59:50 -0300

    Add authentication to update and delete sensor routes

--- backend/routes/sensorRoutes.js
+++ backend/routes/sensorRoutes.js
@@ -1,9 +1,12 @@
 import express from "express";
+import { autenticar } from "../auth.js";
 import sensorController from "../controller/sensorController.js";
 
 const router = express.Router();
 
 router.post("/register", sensorController.registerSensor);
 router.get("/", sensorController.listar);
+router.put("/:id", autenticar, sensorController.atualizar);
+router.delete("/:id", autenticar, sensorController.deletar);
 
 export default router;
