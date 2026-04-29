f245cd5912c2d65fad0f67973f8c80ac5f2cc1f8
matheus110506 <matheus110506@hotmail.com>
2026-04-29 20:38:04 -0300

    Use environment variable for server port

--- backend/server.js
+++ backend/server.js
@@ -232,7 +232,7 @@ app.post("/dados", (req, res) => {
 });
 
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT,  '0.0.0.0', () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
